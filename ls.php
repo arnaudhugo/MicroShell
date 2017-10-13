06fbe94236c35319e081f66db82e29f1dfa9a068
arnaud_h <arnaud_h@720ce22c-1725-4da0-b027-f049bdf0dce9>
2017-10-13 17:05:47 +0000

    MaJ

--- ls.php
+++ ls.php
@@ -8,10 +8,12 @@
 // Last update Fri Oct 13 10:08:42 2017 ARNAUD Hugo
 //
 
-function f_ls($commandTxt) // Pb: '*' '/' '@' dans dossier + ordre alphabetiaue
+function f_ls($commandTxt) // ordre alphabetiaue
 {
     $a = 3;
+    $b = 0;
     $dirName = "./";
+    $tab = array();
     while (isset($commandTxt[$a]) != NULL)
     {
         $dirName = $dirName . $commandTxt[$a];
@@ -23,15 +25,46 @@ function f_ls($commandTxt) // Pb: '*' '/' '@' dans dossier + ordre alphabetiaue
         if ($fichier[0] != '.' && $fichier[0] != '..')
         {
             if (is_link($fichier))
-                echo $fichier, "@", "\n";
+                $tab[$b] = $fichier . "@" . "\n";
             else if (is_dir($fichier))
-                echo $fichier, "/", "\n";
+                $tab[$b] = $fichier . "/" . "\n";
             else if (is_executable($fichier))
-                echo $fichier, "*", "\n";
+                $tab[$b] = $fichier . "*" . "\n";
             else
-                echo $fichier, "\n";
+                $tab[$b] = $fichier . "\n";
+            $b = $b + 1;
         }
-
     }
     closedir($dir);
+    f_triTab($tab);
+}
+
+function f_triTab($tab)
+{
+    $tabLen = 0;
+    while (isset($tab[$tabLen]))
+    {
+        $tabLen = $tabLen + 1;
+    }
+    while ($tabLen > 0)
+    {
+        $tabLen = $tabLen - 1;
+        $a = 0;
+        while ($a < $tabLen)
+        {
+            if ($tab[$a] > $tab[$a + 1])
+            {
+                $tmp = $tab[$a];
+                $tab[$a] = $tab[$a + 1];
+                $tab[$a + 1] = $tmp;
+            }
+            $a = $a + 1;
+        }
+    }
+    $a = 0;
+    while (isset($tab[$a]))
+    {
+        echo $tab[$a];
+        $a = $a + 1;
+    }
 }
