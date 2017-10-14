f2493921aa83235ed68c3e3167e3cad4a2bf58fa
arnaud_h <arnaud_h@720ce22c-1725-4da0-b027-f049bdf0dce9>
2017-10-14 09:53:45 +0000

    Add cd setenv unsetenv

---
+++ MicroShell/arnaud_h/cd.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+// cd.php for microshell in /home/arnaud/arnaud_h
+// 
+// Made by ARNAUD Hugo
+// Login   <arnaud_h@etna-alternance.net>
+// 
+// Started on  Sat Oct 14 11:44:40 2017 ARNAUD Hugo
+// Last update Sat Oct 14 11:46:32 2017 ARNAUD Hugo
+//
+
+function functcd($commandTxt)
+{
+  echo "cd: ", "Commande non disponible", "\n";
+}
